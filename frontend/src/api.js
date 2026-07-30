2c4eac31d73fb5b1b08b0a29a542d2b18965e1fa
Ng Ka Hock <fujia643@gmail.com>
2026-07-30 20:08:18 +0800

    Deploying frontend and change URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
 
-    baseURL:"http://127.0.0.1:8000/api/"
+    baseURL:"https://event-management-system-0y0v.onrender.com"
 });
 
 export default API;
